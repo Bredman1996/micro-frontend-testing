b9eabeb0e04bcf0fba6e7e060019c14db435b393
Brandon <bredman@1800contacts.com>
2020-10-14 11:59:23 -0600

    progress? pogress? poggers? maybe

--- container/src/assets/startup.js
+++ container/src/assets/startup.js
@@ -2,53 +2,50 @@
 let apps =
   [
     {
-      "name": "child-one",
-      "activeWhen":"/child-one",
-      "url": "http://localhost:4201/",
-      "bundleMapsUrl":"http://localhost:4201/assets/bundle-maps.json",
+      "name": "styleguide",
+      "activeWhen": "/",
+      "url": "http://localhost:4202/",
+      "bundleMapsUrl": "http://localhost:4202/assets/bundle-maps.json",
       "mainBundleName": ""
     },
     {
-      "name":"styleguide",
-      "activeWhen":"/",
-      "url":"http://localhost:4202",
-      "bundleMapsUrl":"http://localhost:4202/assets/bundle-maps.json",
-      "mainBundleName":""
-    }
+      "name": "child-one",
+      "activeWhen": "/child-one",
+      "url": "http://localhost:4201/",
+      "bundleMapsUrl": "http://localhost:4201/assets/bundle-maps.json",
+      "mainBundleName": ""
+    },
   ];
 
 let appLoadPromises = [];
-System.import('@angular/common').then(() => {
 
-  apps.forEach((appInfo) => {
-    appLoadPromises.push(new Promise((resolve, reject) => {
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open("GET", `${appInfo.bundleMapsUrl}`);
-      xmlHttp.setRequestHeader('cache-control', 'no-cache, must-revalidate, post-check=0, pre-check=0');
-      xmlHttp.setRequestHeader('cache-control', 'max-age=0');
-      xmlHttp.setRequestHeader('expires', '0');
-      xmlHttp.setRequestHeader('expires', 'Tue, 01 Jan 1980 1:00:00 GMT');
-      xmlHttp.setRequestHeader('pragma', 'no-cache');
-      xmlHttp.send(null);
-      xmlHttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          let bundleMaps = JSON.parse(xmlHttp.responseText);
-          console.log(bundleMaps.main);
-          appInfo.mainBundleName = bundleMaps.main;
-          resolve();
-        }
-      };
-    }))
-  })
+apps.forEach((appInfo) => {
+  appLoadPromises.push(new Promise((resolve, reject) => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", `${appInfo.bundleMapsUrl}`);
+    xmlHttp.setRequestHeader('cache-control', 'no-cache, must-revalidate, post-check=0, pre-check=0');
+    xmlHttp.setRequestHeader('cache-control', 'max-age=0');
+    xmlHttp.setRequestHeader('expires', '0');
+    xmlHttp.setRequestHeader('expires', 'Tue, 01 Jan 1980 1:00:00 GMT');
+    xmlHttp.setRequestHeader('pragma', 'no-cache');
+    xmlHttp.send(null);
+    xmlHttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let bundleMaps = JSON.parse(xmlHttp.responseText);
+        appInfo.mainBundleName = bundleMaps.main;
+        resolve();
+      }
+    };
+  }))
 });
-  
-  let importmaps = {
-    "imports":{
-      
-    }
+
+let importmaps = {
+  "imports": {
+
   }
-  
-  Promise.all(appLoadPromises).then(() => {
+}
+
+Promise.all(appLoadPromises).then(() => {
   apps.forEach(app => {
     importmaps.imports[app.name] = `${app.url}/${app.mainBundleName}`;
   });
@@ -58,7 +55,7 @@ System.import('@angular/common').then(() => {
   document.head.appendChild(script);
   System.import('single-spa').then((singleSpa) => {
     const { registerApplication, start } = singleSpa;
-    apps.forEach( (app) => {
+    apps.forEach((app) => {
       registerApplication({
         name: app.name,
         app: () => System.import(app.name),
--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,3 +1,40 @@
 module.exports = {
-    externals: [/^@angular/]
+    externals: [{
+        "@angular/compiler": {
+            "path": "https://cdn.jsdelivr.net/npm/@angular/compiler@10.1.4/bundles/compiler.umd.min.js",
+            "globalName": "@angular/compiler"
+        },
+        "@angular/common": {
+            "path": "https://cdn.jsdelivr.net/npm/@angular/common@10.1.4/bundles/common.umd.min.js",
+            "globalName": "@angular/common"
+        },
+        "@angular/router": {
+            "path": "https://cdn.jsdelivr.net/npm/@angular/router@10.1.4/bundles/router.umd.min.js",
+            "globalName": "@angular/router"
+        },
+        "rxjs":{
+            "path":"https://unpkg.com/rxjs@6.6.0/bundles/rxjs.umd.min.js",
+            "globalName":"rxjs"
+        },
+        "@angular/core": {
+            "path:": "https://unpkg.com/@angular/core@10.1.4/bundles/core.umd.min.js",
+            "globalName": "@angular/core"
+        },
+        // "@angular/platform-browser":{
+        //     "path":"https://cdn.jsdelivr.net/npm/@angular/platform-browser@10.1.4/bundles/platform-browser.umd.js",
+        //     "globalName":"@angular/platform-browser"
+        // },
+        // "@angular/platform-browser-dynamic":{
+        //     "path":"https://cdn.jsdelivr.net/npm/@angular/platform-browser-dynamic@10.1.4/bundles/platform-browser-dynamic.umd.js",
+        //     "globalName":"@angular/platform-browser-dynamic"
+        // },
+        // "tslib":{
+        //     "path":"https://cdnjs.cloudflare.com/ajax/libs/tslib/2.0.0/tslib.min.js",
+        //     "globalName":"tslib"
+        // },
+        // "@angular/animations":{
+        //     "path":"https://cdn.jsdelivr.net/npm/@angular/animations@10.1.4/bundles/animations.umd.js",
+        //     "globalName":"@angular/animations"
+        // },
+    }]
 }
